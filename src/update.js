aa1b6fb370546c2f709bc8848dcbe1440072c5b6
Fa Dzikri <109416385+fadzikri@users.noreply.github.com>
2023-01-21 16:37:41 +0700

    write_main_trigger_if_update_data

---
+++ src/update.js
@@ -0,0 +1,4 @@
+import { getEmojiRawDataVersion, createJSONEmojiVersion } from "./versions/versions.js"
+
+setTimeout(() => getEmojiRawDataVersion(), 0);
+setTimeout(() => createJSONEmojiVersion(), 5000);

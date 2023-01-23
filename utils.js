8809ed903b1ccbf9667600f4abb5b57bf003215b
Fa Dzikri <109416385+fadzikri@users.noreply.github.com>
2023-01-23 14:04:13 +0700

    add_new_config

--- utils.js
+++ utils.js
@@ -1,9 +1,15 @@
 class Utils {
     static apiURL = "https://apple.cyclic.app";
     static baseURL = "https://unicode.org/Public/emoji";
-    static nameFilename = "filename";
+    static nameFilename = "filenames";
     static nameFileEmoji = "emoji";
+    static nameFileEmojiPath = "emoji-paths";
     static nameFileVersion = "emoji-versions";
+    static priorityEmojiFile = {
+        high: "emoji-test.txt",
+        medium: "emoji-sequences.txt",
+        low: "emoji-data.txt"
+    }
 }
 
 export default Utils;

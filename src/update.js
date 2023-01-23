42d5972a28b7a9669c798e7ad3f5cf2278bad07d
Fa Dzikri <109416385+fadzikri@users.noreply.github.com>
2023-01-23 14:04:04 +0700

    create_trigger_update

--- src/update.js
+++ src/update.js
@@ -1,4 +1,7 @@
 import { getEmojiRawDataVersion, createJSONEmojiVersion } from "./versions/versions.js"
+import { getEmojisPathTxt, createJSONEmojiPathTxt } from "./paths/paths.js"
 
 setTimeout(() => getEmojiRawDataVersion(), 0);
-setTimeout(() => createJSONEmojiVersion(), 5000);
+setTimeout(() => createJSONEmojiVersion(), 10000);
+setTimeout(() => getEmojisPathTxt(), 20000);
+setTimeout(() => createJSONEmojiPathTxt(), 30000);

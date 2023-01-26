7763c1618a5cbb6dee82abebf64003141818f5e4
Fa Dzikri <109416385+fadzikri@users.noreply.github.com>
2023-01-26 15:07:38 +0700

    add_emoji_json_trigger

--- src/update.js
+++ src/update.js
@@ -1,7 +1,10 @@
 import { getEmojiRawDataVersion, createJSONEmojiVersion } from "./versions/versions.js"
 import { getEmojisPathTxt, createJSONEmojiPathTxt } from "./paths/paths.js"
+import { getEmojiTxt, createJSONEmoji } from "./emojis/emojis.js"
 
 setTimeout(() => getEmojiRawDataVersion(), 0);
 setTimeout(() => createJSONEmojiVersion(), 10000);
 setTimeout(() => getEmojisPathTxt(), 20000);
 setTimeout(() => createJSONEmojiPathTxt(), 30000);
+setTimeout(() => getEmojiTxt(), 40000);
+setTimeout(() => createJSONEmoji(), 50000);

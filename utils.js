e2adc0b7f9607f0be5a2640580cc662a0560377d
Fa Dzikri <109416385+fadzikri@users.noreply.github.com>
2023-01-26 15:07:23 +0700

    add_dirs_config

--- utils.js
+++ utils.js
@@ -1,8 +1,10 @@
+import path from "path";
+
 class Utils {
     static apiURL = "https://apple.cyclic.app";
     static baseURL = "https://unicode.org/Public/emoji";
     static nameFilename = "filenames";
-    static nameFileEmoji = "emoji";
+    static nameFileEmoji = "emojis";
     static nameFileEmojiPath = "emoji-paths";
     static nameFileVersion = "emoji-versions";
     static priorityEmojiFile = {
@@ -12,4 +14,11 @@ class Utils {
     }
 }
 
-export default Utils;
+class Dirs {
+    static dirSrc = `${path.resolve()}/src`;
+    static dirVersion = `${this.dirSrc}/versions`;
+    static dirPath = `${this.dirSrc}/paths`;
+    static dirEmoji = `${this.dirSrc}/emojis`;
+}
+
+export { Utils, Dirs };

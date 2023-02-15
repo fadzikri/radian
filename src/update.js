7e0e82982edabb59fe7fb0efe48b68226a76d7d4
Fa Dzikri <109416385+fadzikri@users.noreply.github.com>
2023-02-15 07:06:56 +0700

    refactor_and_test_trigger_function

--- src/update.js
+++ src/update.js
@@ -1,10 +1,10 @@
-import {getEmojiRawDataVersion, createJSONEmojiVersion} from './versions/versions.js';
-import {getEmojisPathTxt, createJSONEmojiPathTxt} from './paths/paths.js';
-import {getEmojiTxt, createJSONEmoji} from './emojis/emojis.js';
+const {getHTMLEmojiVersion, createJSONEmojiVersion} = require('./versions/versions');
+const {getHTMLEmojisPath, createJSONEmojiPath} = require('./paths/paths');
+// const {getEmojiTxt, createJSONEmoji} = require('./emojis/emojis');
 
-setTimeout(() => getEmojiRawDataVersion(), 0);
+setTimeout(() => getHTMLEmojiVersion(), 0);
 setTimeout(() => createJSONEmojiVersion(), 10000);
-setTimeout(() => getEmojisPathTxt(), 20000);
-setTimeout(() => createJSONEmojiPathTxt(), 30000);
-setTimeout(() => getEmojiTxt(), 40000);
-setTimeout(() => createJSONEmoji(), 50000);
+setTimeout(() => getHTMLEmojisPath(), 20000);
+setTimeout(() => createJSONEmojiPath(), 30000);
+// setTimeout(() => getEmojiTxt(), 40000);
+// setTimeout(() => createJSONEmoji(), 50000);

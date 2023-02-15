9d0432f3989a975db4200f9bdbb3c2ee626120e7
Fa Dzikri <109416385+fadzikri@users.noreply.github.com>
2023-02-15 07:07:11 +0700

    refactor_and_renew_variables_name

--- src/paths/paths.js
+++ src/paths/paths.js
@@ -1,28 +1,28 @@
 const fs = require('fs');
-const {Dirs, Utils} = require('../../utils.js');
 const pretty = require('pretty');
 const {load} = require('cheerio');
+const {priority} = require('../../utils');
 
-const {baseURL} = Utils;
-const dirEmojiPathHTML = `${Dirs.dirPath}/${Utils.nameFileEmoji}`;
-const fileEmojiVersionJSON = `${Dirs.dirVersion}/${Utils.nameFileVersion}.json`;
+const emojiVersions = `${__dirname}/../versions/emoji-versions.json`;
+let numVers = null;
 
-const getEmojisPathTxt = () => {
-	fs.readFile(fileEmojiVersionJSON, (err, data) => {
+const getHTMLEmojisPath = () => {
+	fs.readFile(emojiVersions, (err, data) => {
 		if (err) {
 			return console.log('Error when reading file JSON');
 		}
 
 		const parsedData = JSON.parse(data);
-		const name = parsedData.versions;
-		const {links} = parsedData;
+		const {versions: numVersion, links} = parsedData;
+
+		numVers = numVersion;
 
 		try {
 			links.forEach(async (link, i) => {
-				const rawData = await fetch(`${baseURL}/${link}`);
-				const textData = await rawData.text();
+				const webPage = await fetch(link);
+				const htmlPage = await webPage.text();
 
-				fs.writeFile(`${dirEmojiPathHTML}-${name[i]}.html`, pretty(textData), 'utf-8', err => {
+				fs.writeFile(`${__dirname}/emojis-${numVers[i]}.html`, pretty(htmlPage), 'utf-8', err => {
 					if (err) {
 						console.log(err);
 					} else {
@@ -37,60 +37,53 @@ const getEmojisPathTxt = () => {
 	});
 };
 
-const createJSONEmojiPathTxt = () => {
-	const emojisVersionRaw = fs.readFileSync(fileEmojiVersionJSON);
-	const emojisVersionData = JSON.parse(emojisVersionRaw);
-	const emojiVersion = emojisVersionData.versions;
-
-	fs.readFile(`${Dirs.dirVersion}/${Utils.nameFilename}.json`, 'utf-8', (err, data) => {
+const createJSONEmojiPath = () => {
+	fs.readFile(`${__dirname}/../versions/filenames.json`, 'utf-8', (err, data) => {
 		if (err) {
 			return console.log('Error when reading JSON file.');
 		}
 
 		const files = JSON.parse(data);
 
-		const arrayFilesEmojiTXTs = [];
+		const result = [];
 
 		files.forEach((file, i) => {
-			const arrayFilesEmojiTXT = {};
+			const arrayPath = {};
 
-			const $ = load(fs.readFileSync(`${Dirs.dirPath}/${file}`));
-			let fileTXTs = $('td a');
-			fileTXTs = $(fileTXTs).slice(1, fileTXTs.length);
+			const $ = load(fs.readFileSync(`${__dirname}/${file}`));
+			let fileEmojis = $('td a');
+			fileEmojis = $(fileEmojis).slice(1, fileEmojis.length);
 
-			let arrayFilesEmojiSelection = null;
+			let fileEmoji = null;
 
-			fileTXTs.each((i, fileTXT) => {
+			fileEmojis.each((i, fileTXT) => {
 				const fileTxt = $(fileTXT).text();
 
-				if (Utils.prioritye.low === fileTxt) {
-					arrayFilesEmojiSelection = fileTxt;
+				if (priority.low === fileTxt) {
+					fileEmoji = fileTxt;
 				} else
-				if (Utils.priority.medium === fileTxt) {
-					arrayFilesEmojiSelection = fileTxt;
+				if (priority.medium === fileTxt) {
+					fileEmoji = fileTxt;
 				} else
-				if (Utils.priority.high === fileTxt) {
-					arrayFilesEmojiSelection = fileTxt;
+				if (priority.high === fileTxt) {
+					fileEmoji = fileTxt;
 				}
 			});
 
-			arrayFilesEmojiTXT.version = emojiVersion[i];
-			arrayFilesEmojiTXT.filename = arrayFilesEmojiSelection;
+			arrayPath.version = numVers[i];
+			arrayPath.filename = fileEmoji;
 
-			arrayFilesEmojiTXTs.push(arrayFilesEmojiTXT);
+			result.push(arrayPath);
 		});
 
-		fs.writeFile(`${Dirs.dirPath}/${Utils.nameFileEmojiPath}.json`,
-			JSON.stringify(arrayFilesEmojiTXTs),
-			'utf-8',
-			err => {
-				if (err) {
-					console.log('Error when create JSON file.');
-				} else {
-					console.log('Emoji Path JSON Created!');
-				}
-			});
+		fs.writeFile(`${__dirname}/emoji-paths.json`, JSON.stringify(result), 'utf-8', err => {
+			if (err) {
+				console.log('Error when create JSON file.');
+			} else {
+				console.log('Emoji Path JSON Created!');
+			}
+		});
 	});
 };
 
-module.exports = {getEmojisPathTxt, createJSONEmojiPathTxt};
+module.exports = {getHTMLEmojisPath, createJSONEmojiPath};

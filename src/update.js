68843336c7fddf05b733f9a89a141a32afe7ef88
Fa Dzikri <109416385+fadzikri@users.noreply.github.com>
2023-02-16 07:35:49 +0700

    add_new_trigger

--- src/update.js
+++ src/update.js
@@ -1,10 +1,10 @@
 const {getHTMLEmojiVersion, createJSONEmojiVersion} = require('./versions/versions');
 const {getHTMLEmojisPath, createJSONEmojiPath} = require('./paths/paths');
-// const {getEmojiTxt, createJSONEmoji} = require('./emojis/emojis');
+const {getHTMLEmojiText, createJSONEmojiText} = require('./emojis/emojis');
 
 setTimeout(() => getHTMLEmojiVersion(), 0);
 setTimeout(() => createJSONEmojiVersion(), 10000);
 setTimeout(() => getHTMLEmojisPath(), 20000);
 setTimeout(() => createJSONEmojiPath(), 30000);
-// setTimeout(() => getEmojiTxt(), 40000);
-// setTimeout(() => createJSONEmoji(), 50000);
+setTimeout(() => getHTMLEmojiText(), 40000);
+// setTimeout(() => createJSONEmojiText(), 50000);

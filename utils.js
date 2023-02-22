c38c0fe8f80e003579e997712b05b0cfdb7fb4fd
Fa Dzikri <109416385+fadzikri@users.noreply.github.com>
2023-02-22 19:19:29 +0700

    Refactor API URL based on Environtment

--- utils.js
+++ utils.js
@@ -1,4 +1,14 @@
-const apiURL = 'https://radian.vercel.app';
+require('dotenv').config();
+
+let apiURL = null;
+
+if (process.env.NODE_ENV === 'development') {
+	apiURL = 'http://localhost:3000';
+} else {
+	apiURL = 'https://radian.vercel.app';
+}
+
+const port = process.env.PORT;
 const baseURL = 'https://unicode.org/Public/emoji';
 const priority = {
 	high: 'emoji-test.txt',
@@ -6,4 +16,4 @@ const priority = {
 	low: 'emoji-data.txt',
 };
 
-module.exports = {apiURL, baseURL, priority};
+module.exports = {port, apiURL, baseURL, priority};

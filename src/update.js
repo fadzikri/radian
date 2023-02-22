3db7271dd3fc91651ff981ffff05bc1816eeef35
Fa Dzikri <109416385+fadzikri@users.noreply.github.com>
2023-02-22 21:33:01 +0700

    Add Last Update Trigger

--- src/update.js
+++ src/update.js
@@ -1,6 +1,7 @@
 const {getHTMLEmojiVersion, createJSONEmojiVersion} = require('./versions/versions');
 const {getHTMLEmojisPath, createJSONEmojiPath} = require('./paths/paths');
 const {getHTMLEmojiText, createJSONEmojiText, createJSONEmojiFileName} = require('./emojis/emojis');
+const markLastUpdate = require('./last_update/last_update');
 
 setTimeout(() => getHTMLEmojiVersion(), 0);
 setTimeout(() => createJSONEmojiVersion(), 8000);
@@ -9,3 +10,4 @@ setTimeout(() => createJSONEmojiPath(), 29000);
 setTimeout(() => getHTMLEmojiText(), 35000);
 setTimeout(() => createJSONEmojiText(), 48000);
 setTimeout(() => createJSONEmojiFileName(), 55000);
+setTimeout(() => markLastUpdate(), 60000);

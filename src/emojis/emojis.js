5b4c23d9dfdb90393c907c4d487e13efe815bb4a
Fa Dzikri <109416385+fadzikri@users.noreply.github.com>
2023-02-23 17:14:23 +0700

    Add Global Flag to Make All

--- src/emojis/emojis.js
+++ src/emojis/emojis.js
@@ -49,7 +49,7 @@ const createJSONEmojiText = () => {
 			arraysOfUnicode.forEach(unicode => {
 				const resultEmoji = {};
 				const getUnicode = unicode.replace(/;|#/g, '').trim();
-				const createEmoji = String.fromCodePoint(parseInt(getUnicode.replace(/\s/, ''), 16));
+				const createEmoji = String.fromCodePoint(parseInt(getUnicode.replace(/\s/g, ''), 16));
 
 				resultEmoji.emoji = createEmoji;
 				resultEmoji.unicode = getUnicode;

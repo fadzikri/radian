8854e0aa56d5d5e7d2d1c57984b934bbf126d6db
Fa Dzikri <109416385+fadzikri@users.noreply.github.com>
2023-02-26 19:39:43 +0700

    Add Time Trigger

--- src/update.js
+++ src/update.js
@@ -4,10 +4,10 @@ const {getHTMLEmojiText, createJSONEmojiText, createJSONEmojiFileName} = require
 const markLastUpdate = require('./last_update/last_update');
 
 setTimeout(() => getHTMLEmojiVersion(), 0);
-setTimeout(() => createJSONEmojiVersion(), 8000);
-setTimeout(() => getHTMLEmojisPath(), 18000);
-setTimeout(() => createJSONEmojiPath(), 29000);
-setTimeout(() => getHTMLEmojiText(), 35000);
-setTimeout(() => createJSONEmojiText(), 48000);
-setTimeout(() => createJSONEmojiFileName(), 55000);
-setTimeout(() => markLastUpdate(), 60000);
+setTimeout(() => createJSONEmojiVersion(), 10000);
+setTimeout(() => getHTMLEmojisPath(), 20000);
+setTimeout(() => createJSONEmojiPath(), 35000);
+setTimeout(() => getHTMLEmojiText(), 40000);
+setTimeout(() => createJSONEmojiText(), 55000);
+setTimeout(() => createJSONEmojiFileName(), 65000);
+setTimeout(() => markLastUpdate(), 70000);
